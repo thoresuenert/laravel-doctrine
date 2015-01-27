f24c1e08fd3bcb5cc464f85cba7bb3ec3b02e009
Thore Suenert <mail@thoresuenert.de>
2015-01-27 20:05:18 +0100

    test done

--- src/LaravelDoctrineServiceProvider.php
+++ src/LaravelDoctrineServiceProvider.php
@@ -88,7 +88,6 @@ class LaravelDoctrineServiceProvider extends ServiceProvider
     {
         $this->app->singleton(EntityManager::class, function ($app) {
             $config = $app['config']['doctrine'];
-            dd($config);
             // workbench: __DIR__.'/..';
 //            $basePath = __DIR__.'/..';
             $basePath = $app['path.base'];
